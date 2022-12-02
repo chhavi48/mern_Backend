2eb6cdbdbbf3e0e92255288361575c2aafa35ac9
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-12-02 10:57:42 +0530

    env

--- Config/db.js
+++ Config/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.MONGO_DB_URL);
+const connection = mongoose.connect(process.env.MONGO_URL);
 
 module.exports = {connection}
